f813e6d8e1adf3cb96050321525716933d6ef696
Amit Bakhru <bakhru@me.com>
2020-08-07 18:08:47 -0700

    init nlp libs comparison

---
+++ nlp_libs/TwitterSpacyVec.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+"""
+Here we will use a large spacy model to calculate document vectors and use it along with
+XGBoost for classification of twitter texts
+"""
+import re
+
+import numpy as np
+import pandas as pd
+import spacy
+from gensim.parsing import remove_stopwords
+from sklearn.metrics import f1_score, roc_auc_score
+from sklearn.model_selection import train_test_split
+from xgboost.sklearn import XGBClassifier
+
+
+def clean_data(text):
+    text = re.sub('@[\w]*', '', text)  # remove @user
+    text = re.sub('&amp;', '', text)  # remove &amp;
+    text = re.sub('[?!.;:,,#@-]', '', text)  # remove special characters
+    text = re.sub(r'[^\x00-\x7F]+', '', text)  # remove Unicode characters
+    text = text.replace("[^A-Za-z#]", "")  # Replace everything except alphabets and hash
+    text = text.lower()  # make everything lowercase for uniformity
+    # removing short words which are of length 3 or lower(eg. hmm, oh) since they dont add any value
+    text = " ".join(w for w in text.split() if len(w) > 3)
+    # removing stop-words eg. 'we', 'our', 'ours', 'ourselves', 'just', 'don', "don't", 'should'
+    text = remove_stopwords(text)
+    return text
+
+
+# *************************  read training data *******************************
+df = pd.read_csv('.//data//train_tweets.csv')
+print(df.head())
+
+df.drop('id', axis=1, inplace=True)
+df.drop_duplicates()
+print(df.isna().sum())
+
+tweets = df['tweet']
+labels = df['label']
+
+# clean the texts
+tweets = tweets.apply(lambda x: clean_data(x))
+
+# Since the data is highly skewed
+SCALE_FACTOR = labels.value_counts()[0] / labels.value_counts()[1]
+
+# we load a large model becuase we need vectors
+nlp = spacy.load("en_core_web_lg")
+
+# **********************  compute the vectors *********************************
+
+# Disabling other pipes because we don't need them and it'll speed up this part a bit
+with nlp.disable_pipes():
+    docs = list(nlp.pipe(tweets))
+    doc_vectors = np.array([doc.vector for doc in docs])
+
+print("doc vectors shape=", doc_vectors.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(doc_vectors, labels, test_size=0.3,
+                                                    random_state=1, stratify=labels)
+
+xgb_model = None
+xgb_model = XGBClassifier(
+    n_estimators=100,
+    scale_pos_weight=SCALE_FACTOR,
+    objective='binary:logistic',
+    colsample=0.9,
+    colsample_bytree=0.5,
+    eta=0.1,
+    max_depth=8,
+    min_child_weight=6,
+    subsample=0.9)
+
+print("Training xgb model....")
+xgb_model.fit(X_train, y_train)
+
+print("score=", xgb_model.score)
+
+preds = xgb_model.predict(X_test)
+
+print("f1 score=", f1_score(preds, y_test))
+
+# we get an f1 score of arroud 0.64
+
+print("ROC AUC score = ", roc_auc_score(y_test, preds))
---
+++ nlp_libs/TwitterTextBlob.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+"""
+In this program we will use WordBlob to classify tweets as racist or not
+"""
+
+import re
+
+import pandas as pd
+from gensim.parsing import remove_stopwords
+from sklearn.metrics import f1_score, roc_auc_score
+from sklearn.model_selection import train_test_split
+from textblob import classifiers
+
+
+def clean_data(text):
+    text = re.sub('@[\w]*', '', text)  # remove @user
+    text = re.sub('&amp;', '', text)  # remove &amp;
+    text = re.sub('[?!.;:,,#@-]', '', text)  # remove special characters
+    text = re.sub(r'[^\x00-\x7F]+', '', text)  # remove Unicode characters
+    text = text.replace("[^A-Za-z#]", "")  # Replace everything except alphabets and hash
+    text = text.lower()  # make everything lowercase for uniformity
+    # removing short words which are of length 3 or lower(eg. hmm, oh) since they dont add any value
+    text = " ".join(w for w in text.split() if len(w) > 3)
+    # removing stop-words eg. 'we', 'our', 'ours', 'ourselves', 'just', 'don', "don't", 'should'
+    text = remove_stopwords(text)
+    return text
+
+
+# *************************  read training data *******************************
+df = pd.read_csv('.//data//train_E6oV3lV.csv')
+print(df.head())
+
+df.drop('id', axis=1, inplace=True)
+df.drop_duplicates()
+print(df.isna().sum())
+
+df = df[:5000]  # using the whole dataset hangs the system
+
+tweets = df['tweet']
+labels = df['label']
+
+# clean the data set
+tweets = tweets.apply(lambda x: clean_data(x))
+
+tweets = tweets.tolist()
+labels = labels.tolist()
+
+# *****************************************************************************
+tweets_train, tweets_test, labels_train, labels_test = train_test_split(tweets, labels,
+                                                                        test_size=0.3,
+                                                                        random_state=100,
+                                                                        stratify=labels)
+
+# We need to convert the train and test data into a list of tuples of the type (tweet,label)
+training_corpus = list(zip(tweets_train, labels_train))
+test_corpus = list(zip(tweets_test, labels_test))
+
+print("Training classifier......")
+classifier = classifiers.DecisionTreeClassifier(training_corpus)
+
+predictions = []
+for tweet in tweets_test:
+    pred = classifier.classify(tweet)
+    predictions.append(pred)
+
+print("F1 score=", f1_score(labels_test, predictions))
+print("ROC AUC score = ", roc_auc_score(labels_test, predictions))
+
+# we get F1 score=0.4662576687116564
