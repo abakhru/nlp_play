8e9c4e68c0bf7c5725cf946ae38d4d93a4b9ae7f
Amit Bakhru <bakhru@me.com>
2020-08-07 18:09:05 -0700

    adds support for building venv and installing required dependencies

---
+++ quick_start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+set -xe
+export WORKSPACE=${WORKSPACE:-$PWD}
+export PYTHONPATH=${WORKSPACE}
+export PYTHONIOENCODING=utf-8
+
+cd "${WORKSPACE}"
+if [ ! -d "${WORKSPACE}"/env ]; then
+  python3 -m venv "${WORKSPACE}"/env
+fi
+source "${WORKSPACE}/env/bin/activate"
+
+"${VIRTUAL_ENV}"/bin/pip install -U pip setuptools wheel
+"${VIRTUAL_ENV}"/bin/pip install -e .
+
+. "${VIRTUAL_ENV}"/bin/activate
